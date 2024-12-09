dff52b6c43ab0aaa06490535ad23b025fb308b9c
emeraldrains <43196930+emeraldrains@users.noreply.github.com>
2024-12-09 16:14:34 -0500

    set up

--- mymain.py
+++ mymain.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import polars as pl
 
 # scipy
@@ -22,6 +21,9 @@ import polars as pl
 seed = 3794631
 np.random.seed(seed)
 
+train = pl.read_csv("train.csv")
+test = pl.read_csv("test.csv")
+Y_test = pl.read_csv("test_y.csv")
 
 # 1. Build a Binary Classification Model
 
@@ -31,10 +33,21 @@ np.random.seed(seed)
 
 # logistic regression
 
-2. Interpretability Analysis
-Using split 1 and the corresponding trained model, implement an interpretability approach to identify which parts of each review have an impact on the sentiment prediction. Apply your method to 5 randomly selected positive reviews and 5 randomly selected negative reviews from the split 1 test data.
 
-Set a random seed before selecting these 10 reviews (the seed does not need to relate to students’ UINs).
 
-Provide visualizations (such as highlighted text) that show the key parts of a review contributing to the sentiment prediction. Discuss the effectiveness and limitations of the interpretability approach you chose.
+#For the interpretability analysis, a binary classification model trained on split1 should have been saved online, so access to train.csv is no longer required. If the ten randomly selected test samples are saved online, access to the original test.csv with OpenAI embeddings also is no longer required. But you may need online access to test data with the (non-OpenAI) embeddings you selected.
+
+#we also need to save the linear transform matrix W, which is used to align non-OpenAI embeddings and OpenAI embeddings online
+#As with the ten random samples, you may choose to save W or not. The main advantage of saving is faster execution the next time the code is run, because you won't need to regenerate the samples or W.
+
+
+test.write_csv("mysubmission.csv")
+
+
+# 2. Interpretability Analysis
+# Using split 1 and the corresponding trained model, implement an interpretability approach to identify which parts of each review have an impact on the sentiment prediction. Apply your method to 5 randomly selected positive reviews and 5 randomly selected negative reviews from the split 1 test data.
+
+# Set a random seed before selecting these 10 reviews (the seed does not need to relate to students’ UINs).
+
+# Provide visualizations (such as highlighted text) that show the key parts of a review contributing to the sentiment prediction. Discuss the effectiveness and limitations of the interpretability approach you chose.
 
